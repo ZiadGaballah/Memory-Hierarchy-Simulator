4032d059674385b2d301961cf83bc88c8f64d460
Hhassan5 <90256500+Hhassan5@users.noreply.github.com>
2024-12-07 15:29:25 +0200

    Update cache.cpp

--- cache.cpp
+++ cache.cpp
@@ -7,20 +7,19 @@
 #include <bitset>
 #include <iomanip>
 
-
 using namespace std;
 
 struct CacheLine
 {
     bool validBit;
-    unsigned long long tag;
+    unsigned int tag;
     CacheLine() : validBit(false), tag(0) {}
 };
 
 int main()
 {
     // Input parameters
-    unsigned long long memoryBits;
+    int memoryBits;
     int memoryCycles; // Memory access time in cycles
     int S;            // Total cache size (in bytes)
     int L;            // Cache line size (in bytes)
@@ -42,7 +41,7 @@ int main()
     cin >> fileName;
 
     // Compute the number of cache lines
-    unsigned long long lines = S;
+    int lines = S / L;
     vector<CacheLine> cache(lines);
 
     // Open the file containing the sequence of memory accesses
@@ -57,7 +56,7 @@ int main()
     string line;
     if (!getline(inFile, line))
     {
-        cerr << "Error reading from file or file is empty." << endl;
+        cerr << "file error!" << endl;
         return 1;
     }
     inFile.close();
@@ -65,71 +64,60 @@ int main()
     // Parse the addresses
     stringstream ss(line);
     string addressStr;
-    vector<unsigned long long> addresses;
+    vector<unsigned long long> addresses; // Store decimal addresses
     while (getline(ss, addressStr, ','))
     {
         // Trim possible spaces
         if (!addressStr.empty() && addressStr.find_first_not_of(' ') != string::npos)
         {
-try
-            {
-                // Convert the string address from binary to unsigned long long
-                unsigned long long address = stoull(addressStr, nullptr, 2); // Convert from binary string to unsigned long long
-                addresses.push_back(address);
-            }
-            catch (const invalid_argument& e)
-            {
-                cerr << "Invalid address format: " << addressStr << endl;
-                return 1;
-            }        }
+            addresses.push_back(stoi(addressStr)); // Convert string to decimal (unsigned int)
+        }
     }
 
     // Simulation variables
     int totalAccesses = 0;
     int hits = 0;
     int misses = 0;
-    int blockOffsetBits = log2(L);         // Number of bits to index a byte within a cache line
-    int indexBits = log2(lines);           // Number of bits to index the cache lines
+    int blockOffsetBits = log2(L); // Number of bits to index a byte within a cache line
+    int indexBits = log2(lines);   // Number of bits to index the cache lines
+    // int tagBits = memoryBits - indexBits - blockOffsetBits; // Not used, can be removed
 
     // Simulation
     for (auto addr : addresses)
     {
         totalAccesses++;
-        
-        // // Convert the binary string to unsigned int using memoryBits for bit length
-        // unsigned long long addr = bitset<40>(addressStr).to_ulong();  // Convert the binary string to unsigned int
-        // if (addressStr.length() > memoryBits)
-        // {
-        //     cerr << "Address in the input exceeds the defined memory bits." << endl;
-        //     return 1;
-        // }
-        
+
         // Compute index and tag
-        unsigned long long blockOffset = addr % L; // Block offset (address % cache line size)
-        unsigned long long index = ((addr) >> blockOffsetBits) % lines; // Index of cache line
-        unsigned long long tag = addr >> (indexBits + blockOffsetBits); // Tag (shift the address to the right)
+        unsigned int blockOffset = addr % L;                      // Block offset (address % cache line size)
+        unsigned int index = (addr / L) % lines;                  // Index of cache line
+        unsigned int tag = addr >> (indexBits + blockOffsetBits); // Tag (shift the address to the right)
 
         bool isHit = false;
 
         // Check the cache line
         if (cache[index].validBit && cache[index].tag == tag)
         {
-            // Hit
             isHit = true;
             hits++;
         }
         else
         {
-            // Miss
             misses++;
-            // Update cache line
             cache[index].validBit = true;
             cache[index].tag = tag;
         }
 
-        // After each access, print the required info
-        cout << "Access #:" << totalAccesses << endl << "Address:      " << bitset<40>(addr) << endl;
-        cout <<"Displacement: " << bitset<40>(blockOffset) << endl << "Index:        " << bitset<40>(index) << endl << "Tag:          " << bitset<40>(tag) << endl;
+        int tagBitLength = memoryBits - indexBits - blockOffsetBits;
+
+        // print for each access
+        cout << "Access number " << totalAccesses << endl
+             << "Decimal Address: " << addr << " (Binary: " << bitset<40>(addr).to_string().substr(40 - memoryBits) << ")" << endl;
+        cout << "Displacement:    " << blockOffset
+             << " (Binary: " << bitset<40>(blockOffset).to_string().substr(40 - blockOffsetBits) << ")" << endl;
+        cout << "Index:           " << index
+             << " (Binary: " << bitset<40>(index).to_string().substr(40 - indexBits) << ")" << endl;
+        int currentTagBits = min(tagBitLength, 40);
+        cout << "Tag:             " << tag << " (Binary: " << bitset<40>(tag).to_string().substr(40 - currentTagBits) << ")" << endl;
         if (isHit)
         {
             cout << "Result: HIT" << endl;
@@ -144,21 +132,18 @@ try
         cout << "-------------------------" << endl;
     }
 
-    // End of simulation results
+    // final results
     cout << "\nFinal Cache State:\n";
     for (int i = 0; i < lines; i++)
     {
-        if (cache[i].validBit == 1)
-        {
         cout << "Line " << i << ": Valid Bit = " << cache[i].validBit
-             << ", Tag = " << bitset<40>(cache[i].tag) << endl;
-        }
+             << ", Tag = " << cache[i].tag
+             << " (Binary: " << bitset<11>(cache[i].tag) << ")" << endl;
     }
 
     double hitRatio = (totalAccesses > 0) ? (double)hits / totalAccesses : 0.0;
     double missRatio = (totalAccesses > 0) ? (double)misses / totalAccesses : 0.0;
 
-    // Compute AMAT
     // AMAT = (Hits * CacheCycles + Misses * (CacheCycles + MemoryCycles)) / TotalAccesses
     double amat = 0.0;
     if (totalAccesses > 0)
